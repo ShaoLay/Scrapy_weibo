22b783208891a6bbc0feb21c760ad2f6cd5be295
hamdi <javs_shao@163.com>
2019-01-28 10:27:38 +0800

    数据清洗_1_转化标准时间

--- weibo/weibo/pipelines.py
+++ weibo/weibo/pipelines.py
@@ -4,8 +4,22 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import re, time
 
 
-class WeiboPipeline(object):
-    def process_item(self, item, spider):
-        return item
+class WeiboPipeline():
+    def parse_time(self, date):
+        if re.match('刚刚', date):
+            date = time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time()))
+        if re.match('\d+分钟前', date):
+            minute = re.match('(\d+)', date).group(1)
+            date = time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time() - float(minute) * 60))
+        if re.match('\d+小时前', date):
+            hour = re.match('(\d+)', date).group(1)
+            date = time.strftime('%Y-%m-%d %H:%M', time.localtime(time.time() - float(hour) * 60 * 60))
+        if re.match('昨天.*', date):
+            date = re.match('昨天(.*)', date).group(1).strip()
+            date = time.strftime('%Y-%m-%d', time.localtime() - 24 * 60 * 60) + ' ' + date
+        if re.match('\d{2}-\d{2}', date):
+            date = time.strftime('%Y-', time.localtime()) + date + ' 00:00'
+        return date
