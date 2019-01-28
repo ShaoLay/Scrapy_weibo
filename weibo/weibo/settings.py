db083af5bcf7ac4af31363e93b2da0367d3f5a4b
hamdi <javs_shao@163.com>
2019-01-28 10:36:34 +0800

    配置文件_启动中间件_Middleware

--- weibo/weibo/settings.py
+++ weibo/weibo/settings.py
@@ -52,9 +52,11 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'weibo.middlewares.WeiboDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   # 'weibo.middlewares.WeiboDownloaderMiddleware': 543,
+'weibo.middlewares.CookiesMiddleware': 554,
+    'weibo.middlewares.ProxyMiddleware': 555,
+}
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
