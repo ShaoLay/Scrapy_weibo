c53de119437569bcc07e863c1628d5435af17bfc
hamdi <javs_shao@163.com>
2019-01-28 10:04:50 +0800

    修改Spider_配置Ajax的URL等信息

--- weibo/weibo/spiders/weibocn.py
+++ weibo/weibo/spiders/weibocn.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
-import scrapy
+from scrapy import Request, Spider
 
 
-class WeibocnSpider(scrapy.Spider):
+class WeibocnSpider(Spider):
     name = 'weibocn'
     allowed_domains = ['m.weibo.cn']
-    start_urls = ['http://m.weibo.cn/']
+    user_url = 'https://m.weibo.cn/api/container/getIndex?uid={uid}&type=uid&value={uid}&containerid=100505{uid}'
+    follow_url = 'https://m.weibo.cn/api/container/getIndex?containerid=231051_-_followers_-_{uid}&page={page}'
+    fan_url = 'https://m.weibo.cn/api/container/getIndex?containerid=231051_-_fans_-_{uid}&page={page}'
+    weibo_url = 'https://m.weibo.cn/api/container/getIndex?uid={uid}&type=uid&page={page}&containerid=107603{uid}'
+    start_users = ['3217179555', '1742566624', '2282991915', '1288739185', '3952070245', '5878659096']
 
-    def parse(self, response):
-        pass
+    def start_requests(self):
+        for uid in self.start_users:
+            yield Request(self.user_url.format(uid=uid), callback=self.parse_user)
+
+    def parse_user(self, response):
+        self.logger.debug(response)
